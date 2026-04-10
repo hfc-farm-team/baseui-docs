ab1fc824feca8a0bbd6837b427f89201d2d005e6
SikongJueluo <selfconfusion@gmail.com>
2026-04-10 23:37:11 +0800

    chore(config): add base path for deployment

--- rspress.config.ts
+++ rspress.config.ts
@@ -1,21 +1,25 @@
-import * as path from 'node:path';
-import { defineConfig } from '@rspress/core';
+import * as path from "node:path";
+import { defineConfig } from "@rspress/core";
 
 export default defineConfig({
-  root: path.join(__dirname, 'docs'),
-  title: 'My Site',
-  icon: '/rspress-icon.png',
-  logo: {
-    light: '/rspress-light-logo.png',
-    dark: '/rspress-dark-logo.png',
-  },
-  themeConfig: {
-    socialLinks: [
-      {
-        icon: 'github',
-        mode: 'link',
-        content: 'https://github.com/web-infra-dev/rspress',
-      },
-    ],
-  },
+    root: path.join(__dirname, "docs"),
+
+    base: "/baseui-docs/",
+
+    title: "My Site",
+    icon: "/rspress-icon.png",
+    logo: {
+        light: "/rspress-light-logo.png",
+        dark: "/rspress-dark-logo.png",
+    },
+
+    themeConfig: {
+        socialLinks: [
+            {
+                icon: "github",
+                mode: "link",
+                content: "https://github.com/web-infra-dev/rspress",
+            },
+        ],
+    },
 });
